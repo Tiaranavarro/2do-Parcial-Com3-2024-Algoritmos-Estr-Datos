89287b13badbbd3f6f3883adb74e039f52be2b69
Tiaranavarro <tiaranavarro0@gmail.com>
2024-06-26 21:15:04 -0300

    Tiara Navarro

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,29 @@
 
+import datetime 
+
+class Alumno: 
+def __init__(self,nombre, dni, fecha_ingreso, carrera):{
+        , self.nombre = nombre
+        , self.dni"= dni
+        , self.fecha_ingreso = datetime.datetime.strptime(fecha_ingreso, '%d/%m/%Y').date()
+        , self.carrera = carrera
+}
+def calcular_antiguedadad(self):
+    if self.fecha_ingreso is None: 
+        (print("No ingreso la fecha de ingreso del alumno"))
+        return.(None)
+    else return.(print(antiguedad))
+
+nombre=input("Ingrese el nombre del alumno")
+
+dni=int(input("Ingrese el DNI del alumno"))
+
+fecha_ingreso = input("Ingrese la fecha de ingreso del alumno(dd/mm/aaaa)")
+
+carrera = input("Ingrese la carrera del alumno") #El alumno solo puede estar anotado en una sola carrera
+
+fecha_de_hoy = datetime.date.today()
+antiguedad = fecha_de_hoy.year - self.["fecha_ingreso"].year
+alumno = Alumno(nombre,dni,fecha_ingreso,carrera)
+print(alumno)
+
--- ejercicio3.py
+++ ejercicio3.py
@@ -1,1 +1,52 @@
+import random
+import datetime
+class Alumno: 
+def __init__(self,nombre, dni, fecha_ingreso, carrera):{
+        , self.nombre = nombre
+        , self.dni"= dni
+        , self.fecha_ingreso = datetime.datetime.strptime(fecha_ingreso, '%d/%m/%Y').date()
+        , self.carrera = carrera
+}
+def calcular_antiguedadad(self):
+    if self.fecha_ingreso is None: 
+        (print("No ingreso la fecha de ingreso del alumno"))
+        return.(None)
+    else return.(print(antiguedad))
+
+nombre=input("Ingrese el nombre del alumno")
+
+dni=int(input("Ingrese el DNI del alumno"))
+
+fecha_ingreso = input("Ingrese la fecha de ingreso del alumno(dd/mm/aaaa)")
+
+carrera = input("Ingrese la carrera del alumno") #El alumno solo puede estar anotado en una sola carrera
+
+fecha_de_hoy = datetime.date.today()
+antiguedad = fecha_de_hoy.year - self.["fecha_ingreso"].year
+alumno = Alumno(nombre,dni,fecha_ingreso,carrera)
+print(alumno)
+
+class Nodo: 
+    def __init__(self.variable=None):
+        self.variable = variable
+        self.proxima_variable= None
+        self.anterior_variable=None
+def agregar_a_la_lista(self.lista_de_nodos):
+    self.lista_de_nodos = None
+    nuevo_nodo = Nodo(variable)
+    while self.lista_de_nodos is not None:
+        self.proxima_variable=nuevo_nodo
+        self.anterior_variable=nodo_antiguo
+def __iter__ (self):
+    self.nuevo_nodo= None
+    self.nuevo_nudo= None
+def __next__ (self):
+    if self.variable is None
+    raise StopIteration
+def lista_ejemplos(self):
+    nombres=["Pedro","Guadalupe""Marta""José"]
+for _ in range(5):
+    nombre = random.choice(nombres)
+    
+
 
